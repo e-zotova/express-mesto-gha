6ee52c5403ad6e7eabf78af0e0c8aea719ab6f02
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-05-04 22:23:52 +0300

    refactor: user router, added index

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const { PORT = 3000 } = process.env;
 const router = require('./routes');
-const userRouter = require('./users');
 const mongoose = require('mongoose');
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT, () => console.log('Сервер запущен.'));
 
 app.use(router);
-app.use(userRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6453dfcb90dc5087cd3d6c7c'
+  };
+
+  next();
+});
+
