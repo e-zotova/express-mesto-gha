1e875834f9127e2f905562c79f0a12ca9246cb23
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-05-04 18:48:36 +0300

    feat: add users controller, model, router

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const { PORT = 3000 } = process.env;
+const router = require('./routes');
+const userRouter = require('./users');
 const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
-
 const app = express();
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
-app.listen(PORT, () => {
-  console.log('Ссылка на сервер');
-});
+app.listen(PORT, () => console.log('Сервер запущен.'));
+
+app.use(router);
+app.use(userRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
---
+++ controllers/users.js
@@ -0,0 +1,40 @@
+const User = require("../models/user");
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({ message: "Server error" }));
+};
+
+const getUserById = (req, res) => {
+  const { id } = req.params;
+
+  User.findById(id)
+    .then((users) => {
+      const user = users.find((item) => item._id === id);
+
+      if (!user) {
+        return res.status(404).send({ message: "User is not found" });
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(() => res.status(500).send({ message: "Server error" }));
+};
+
+const createUser = (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  User.create(data)
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch(() => res.status(500).send({ message: "Server error" }));
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser
+};
