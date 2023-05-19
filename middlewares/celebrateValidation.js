2e263bc3490811c01a035a769a20239adaed703d
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-05-19 15:35:08 +0300

    fix: regex to constant

--- middlewares/celebrateValidation.js
+++ middlewares/celebrateValidation.js
@@ -8,7 +8,7 @@ const validateCreateUser = celebrate(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
+      avatar: Joi.string().regex(urlRegex),
     }),
   },
 );
